181c40b8ae6ba1976af1c2dd3058d7f3dab9fa26
Gaelle-M <gaelle-mazzali@hotmail.fr>
2026-01-10 19:09:41 +0100

    modification du réglage de la navbar

--- src/layout/navbar/Navbar.jsx
+++ src/layout/navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { HiMenuAlt3, HiX } from 'react-icons/hi';
+import { NavHashLink } from 'react-router-hash-link'; // On garde juste cet import
 import './Navbar.scss';
 
 const Navbar = () => {
@@ -16,13 +17,14 @@ const Navbar = () => {
           {isOpen ? <HiX /> : <HiMenuAlt3 />}
         </div>
 
-        {/* Liens de navigation */}
+        {/* Liens de navigation - Utilisation de NavHashLink pour la compatibilité */}
         <ul className={`navbar__links ${isOpen ? 'active' : ''}`}>
-          <li><a href="#" onClick={() => setIsOpen(false)}>Accueil</a></li>
-          <li><a href="#about" onClick={() => setIsOpen(false)}>À propos</a></li>
-          <li><a href="#expertises" onClick={() => setIsOpen(false)}>Expertises</a></li>
-          <li><a href="#projets" onClick={() => setIsOpen(false)}>Projets</a></li>
-          <li><a href="#contact" onClick={() => setIsOpen(false)}>Contact</a></li>
+          <li><NavHashLink smooth to="/#hero" onClick={() => setIsOpen(false)}>Accueil</NavHashLink></li>
+          <li><NavHashLink smooth to="/#about" onClick={() => setIsOpen(false)}>À propos</NavHashLink></li>
+          <li><NavHashLink smooth to="/#expertises" onClick={() => setIsOpen(false)}>Expertises</NavHashLink></li>
+          <li><NavHashLink smooth to="/#projets" onClick={() => setIsOpen(false)}>Projets</NavHashLink></li>
+          <li><NavHashLink smooth to="/#parcours" onClick={() => setIsOpen(false)}>Parcours</NavHashLink></li>
+          <li><NavHashLink smooth to="/#contact" onClick={() => setIsOpen(false)}>Contact</NavHashLink></li>
         </ul>
       </div>
     </nav>
